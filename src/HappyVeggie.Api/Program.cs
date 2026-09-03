e4e6f7d407a33459dea77b037e92854b5860bace
Ramil Ahmad Malik <55125754+ramil91@users.noreply.github.com>
2026-09-03 18:01:21 +0500

    db oush

--- src/HappyVeggie.Api/Program.cs
+++ src/HappyVeggie.Api/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
